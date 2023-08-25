324b984b619f8d6530cf4baac938b70602230821
Artemon-x58 <artem1145819@gmail.com>
2023-08-25 17:01:18 +0200

    update hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Feedback } from './FeedBack/Feedback';
 import { Statistics } from './Statistics/Statistics';
 import { NotificationMessage } from 'components/NotificationMessage/NotificationMesage';
 import { Title } from './Statistics/Statistics.styled';
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good / countTotalFeedback()) * 100);
+
+  const handleAddScore = type => {
+    const updateState = {
+      good: setGood,
+      neutral: setNeutral,
+      bad: setBad,
+    };
+
+    if (updateState[type]) {
+      updateState[type](prevValue => prevValue + 1);
+    }
   };
 
-  countTotalFeedback = () => {
-    const valuesArray = Object.values(this.state);
-    const totalFeedback = valuesArray.reduce(
-      (acc, currentValue) => acc + currentValue,
-      0
-    );
-    return totalFeedback;
-  };
+  const createOptionsArray = () => ['good', 'neutral', 'bad'];
 
-  countPositiveFeedbackPercentage = () =>
-    Number(((this.state.good / this.countTotalFeedback()) * 100).toFixed(0));
-
-  handleAddScore = type => {
-    this.setState(prevState => {
-      return {
-        [type]: prevState[type] + 1,
-      };
-    });
-  };
-
-  createOptionsArray = () => {
-    const arr = Object.keys(this.state);
-    return arr;
-  };
-
-  render() {
-    return (
-      <div style={{ marginLeft: 'auto', marginRight: 'auto', width: '300px' }}>
-        <Feedback
-          onLeaveFeedback={this.handleAddScore}
-          options={this.createOptionsArray()}
+  return (
+    <div style={{ marginLeft: 'auto', marginRight: 'auto', width: '300px' }}>
+      <Feedback
+        onLeaveFeedback={handleAddScore}
+        options={createOptionsArray()}
+      />
+      <Title>Statistics</Title>
+      {countTotalFeedback() < 1 ? (
+        <NotificationMessage message="There is no feedback" />
+      ) : (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          percentage={countPositiveFeedbackPercentage()}
         />
-        <Title>Statistics</Title>
-        {this.countTotalFeedback() < 1 ? (
-          <NotificationMessage message="There is no feedback" />
-        ) : (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            percentage={this.countPositiveFeedbackPercentage()}
-          />
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
